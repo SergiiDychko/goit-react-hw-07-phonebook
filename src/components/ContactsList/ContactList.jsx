6953d31d2d96f92e86c6e7807e3526b82ac25b2f
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-12-14 12:43:01 +0200

    готово

--- src/components/ContactsList/ContactList.jsx
+++ src/components/ContactsList/ContactList.jsx
@@ -1,15 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { fetchContacts, deleteContact } from '../../redux/operations';
 
 import ContactItem from './ContactItem';
 import { StyledList } from './Styles';
+import Loader from '../Loader/Loader';
 import Notification from '../Notification';
 import Button from '../Button';
-import { useEffect } from 'react';
 
 const ContactList = () => {
   const contacts = useSelector(state => state.contacts.items);
   const filter = useSelector(state => state.filter);
+  const loading = useSelector(state => state.isLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {dispatch(fetchContacts())}, [dispatch])
@@ -21,19 +23,27 @@ const ContactList = () => {
 
   const contactsList = filter ? filteredContacts() : contacts;
 
-  return contactsList.length ? (
-    <StyledList>
-      {contactsList.map(({ id, name, phone }) => (
-        <li className="listItem" key={id}>
-          <ContactItem name={name} number={phone} />
-          <Button title="Delete" onClick={() => dispatch(deleteContact(id))} />
-        </li>
-      ))}
-    </StyledList>
-  ) : (
-    <Notification
-      text={filter ? 'Could not find this name' : 'Contact list is empty'}
-    />
+  return (
+    <>
+      {loading && Loader}
+      {contactsList.length ? (
+      <StyledList>
+        {contactsList.map(({ id, name, phone }) => (
+          <li className="listItem" key={id}>
+            <ContactItem name={name} number={phone} />
+            <Button
+              title="Delete"
+              onClick={() => dispatch(deleteContact(id))}
+            />
+          </li>
+        ))}
+      </StyledList>
+      ) : (
+      <Notification
+        text={filter ? 'Could not find this name' : 'Contact list is empty'}
+      />
+      )};
+    </>
   );
 };
 
