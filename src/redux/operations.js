761742d9b0648d7f5bf3ad8ae0c8e59a4ee4062f
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-12-15 16:05:00 +0200

    зробив baseURL для запитів

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+const instance = axios.create({baseURL: 'https://6398b55bfe03352a94dc01e1.mockapi.io/contacts'})
+
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get(
-        'https://6398b55bfe03352a94dc01e1.mockapi.io/contacts/contacts'
+      const response = await instance.get(
+        '/contacts'
         );
       return response.data;
     } catch (error) {
@@ -19,8 +21,8 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async ({ name, phone }, { rejectWithValue }) => {
     try {
-      const response = await axios.post(
-        'https://6398b55bfe03352a94dc01e1.mockapi.io/contacts/contacts',
+      const response = await instance.post(
+        '/contacts',
         { name, phone }
       );
       return response.data;
@@ -34,8 +36,8 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async function (id, { rejectWithValue }) {
     try {
-      const response = await axios.delete(
-        `https://6398b55bfe03352a94dc01e1.mockapi.io/contacts/contacts/${id}`
+      const response = await instance.delete(
+        `/contacts/${id}`
       );
       return response.data;
     } catch (error) {
